db135d3f3586fa7cd155a701d1b17d54d5180a17
sahil-singh1107 <sahil@LAPTOP-TNOQMCGR>
2024-10-20 05:50:40 +0530

    d

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ dotenv.config();
 const dbUrl = process.env.DATABASE_URL;
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({origin: '*'}))
 app.use(express.urlencoded({ extended: true }));
 
 const {linkRouter} = require("./routes/link")
