e023d4bd0bb64caaa4feedd9060f12506db634ff
sahil-singh1107 <sahil@LAPTOP-TNOQMCGR>
2024-10-20 09:15:02 +0530

    added logic for removing link

--- routes/link.js
+++ routes/link.js
@@ -1,7 +1,8 @@
 const express = require("express")
 
 //const {ensureAuthenticated} = require("../middlewares/Auth")
-const {createLink ,getLinks} = require("../controllers/linkController")
+const {createLink ,getLinks} = require("../controllers/linkController");
+const { linkModel } = require("../db");
 
 const linkRouter = express.Router()
 
@@ -12,6 +13,16 @@ function middleware (req,res,next) {
 
 linkRouter.post("/createLink", createLink)
 linkRouter.post("/getLinks", middleware, getLinks)
+linkRouter.post("/deleteLink", async function (req,res) {
+    const {link} = req.body
+    try {
+        await linkModel.deleteOne({link})
+        return res.status(200);
+    } catch (error) {
+        console.log(error)
+        return res.status(500)
+    }
+})
 
 
 module.exports = {linkRouter: linkRouter}
